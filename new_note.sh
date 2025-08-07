fb774b9fedf7ffe068ab381d5d8a3f4d86ac44e7
Kishu <kishujain.1998@gmail.com>
2025-08-08 00:24:09 +0530

    movie note

--- new_note.sh
+++ new_note.sh
@@ -2,12 +2,10 @@
 
 cd ~/notes/Inbox
 
-cd ~/notes/Inbox/
-
 filename=$(date -d "today" +%d-%b-%y_%H:%M).md
 
 if [[ -z $TMUX ]]; then
   nvim $filename
 else
-  tmux split-window -h -l 30% nvim $filename
+  tmux split-window -h -l 87 nvim $filename
 fi
